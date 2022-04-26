94e5b565a1bc808b2992a5d4e44e71575213e92d
mzouink <zouinkhi.marwan@gmail.com>
2022-04-26 15:37:36 -0400

    cleaning

--- src/main/java/janus/JanusClient.java
+++ src/main/java/janus/JanusClient.java
@@ -3,6 +3,12 @@ package janus;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import org.janus.api.ChunkIndexerGrpc;
+import org.janus.api.DataFormat;
+import org.janus.api.Dataset;
+import org.janus.api.DatasetSpec;
+import org.janus.api.NewDatasetRequest;
+import org.janus.api.NewDatasetResponse;
+import org.janus.api.SessionRequest;
 
 public class JanusClient {
     private static final String path = "localhost";
@@ -18,6 +24,7 @@ public class JanusClient {
                 .usePlaintext()
                 .build();
         this.stub = ChunkIndexerGrpc.newBlockingStub(channel);
+
     }
 
     public static JanusClient get() {
@@ -27,12 +34,51 @@ public class JanusClient {
         return instance;
     }
 
+    public ChunkIndexerGrpc.ChunkIndexerBlockingStub getStub() {
+        return stub;
+    }
+
     public static void main(String[] args) {
 
+        JanusClient.get();
 
-//        Session.newBuilder().set
-//        SumResponse result = stub.calculateSum(SumRequest.newBuilder().setX(x).setY(y).build());
+//        rpc NewDataset(NewDatasetRequest) returns (NewDatasetResponse);
+        NewDatasetResponse result = JanusClient.get().getStub().newDataset(
+                NewDatasetRequest.newBuilder().setSpec(
+                        DatasetSpec.newBuilder()
+                                .setDataset(Dataset.newBuilder().setPath("test_path"))
+                                .setLayout(DataFormat.N5)
+                                .setNumDims(3)
+                                .addDimBits(1000).addDimBits(1000).addDimBits(1000)
+                                .setMetadata("")
+                                .build()).build());
+
+//        rpc OpenSession(SessionRequest) returns (Session);
+        NewDatasetResponse result = JanusClient.get().getStub().openSession(
+                SessionRequest.newBuilder()
+                        .setClient("zouinkhim@hhmi.org")
+                        .setDataset(Dataset.newBuilder().setPath("test_path"))
+                .build())
+
+//        // Commits and closes the session.
+//        rpc CommitSession(CommitRequest) returns (CommitResponse);
 //
-//        System.out.println("got result "+result.getResult());
+//        // Write chunks in a session.
+//        rpc WriteChunks(WriteRequest) returns (WriteResponse);
+//
+//        // Read chunks in a session.
+//        rpc ReadChunks(ReadRequest) returns (Chunks);
+//
+//        // Read chunks from any given version.
+//        rpc ReadVersionChunks(ReadVersionRequest) returns (Chunks);
+//
+//        // ---- For advanced clients that read/write directly to backing store.
+//
+//        // Get location of chunks, i.e., (version, chunk coord) tuples, for set of chunk coords at a given version.
+//        rpc GetVersionChunkRefs(ReadVersionRequest) returns (ChunkRefs);
+//
+//        // Update the Janus server index for chunks that have been altered using direct writes to chunk storage.
+//        rpc MarkIngestedChunks(MarkIngestedRequest) returns (MarkIngestedResponse);
+
     }
 }
